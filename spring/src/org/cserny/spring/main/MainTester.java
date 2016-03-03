2ca648829801b7f2479d231f9679f8127da6a496
Leonardo Cserny <leonardo_cserny@netex.ro>
2016-03-03 14:18:46 +0200

    MAIN adds init files

---
+++ spring/src/org/cserny/spring/main/MainTester.java
@@ -0,0 +1,13 @@
+package org.cserny.spring.main;
+
+/**
+ * Created by user on 03.03.2016.
+ */
+public class MainTester
+{
+    public static void main(String[] args)
+    {
+        Person person = new Person();
+        person.sayHello();
+    }
+}
---
+++ spring/src/org/cserny/spring/main/Person.java
@@ -0,0 +1,12 @@
+package org.cserny.spring.main;
+
+/**
+ * Created by user on 03.03.2016.
+ */
+public class Person
+{
+    public void sayHello()
+    {
+        System.out.println("Hello world!");
+    }
+}
