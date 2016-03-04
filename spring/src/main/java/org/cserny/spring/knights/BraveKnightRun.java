5706a6c16dc363f0a16ffab6013ed00a44910ff8
Leonardo Cserny <leonardo_cserny@netex.ro>
2016-03-04 12:39:44 +0200

    CH01 adds annotated config implementation run

--- spring/src/main/java/org/cserny/spring/knights/BraveKnightRun.java
+++ spring/src/main/java/org/cserny/spring/knights/BraveKnightRun.java
@@ -1,5 +1,7 @@
 package org.cserny.spring.knights;
 
+import org.cserny.spring.knights.config.KnightConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -9,7 +11,11 @@ public class BraveKnightRun
 {
     public static void main(String[] args)
     {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(KnightConfig.class);
+        context.refresh();
+
         Knight knight = context.getBean(Knight.class);
         knight.embarkOnQuest();
         context.close();
